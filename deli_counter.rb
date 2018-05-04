f57b4e622cf27ec23b6b8d9db753e9eb14a7acbb
malorie16 <malorie18@gmail.com>
2018-05-04 19:37:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,8 @@ def line(katz_deli_line)
   if katz_deli_line.length == 0
     puts "The line is currently empty."
   elsif
-    "The line is currently:" katz_deli_line << katz_deli_line.length.times do |name| " #{katz_deli_line.index(name) + 1}. #{name}."
+    "The line is currently:" + katz_deli_line << katz_deli_line.length.times do |name| " #{katz_deli_line.index(name) + 1}. #{name}."
     end
-    
+
   end
 end
