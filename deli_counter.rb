7b473acc56cb05207cd0bc004691467bc9022e84
malorie16 <malorie18@gmail.com>
2018-05-04 19:25:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def line(katz_deli_line)
+  if katz_deli_line.length == 0
+    puts "The line is currently empty."
+  elsif
+    "The line is currently:" katz_deli_line << katz_deli_line.length.times do |name| " #{katz_deli_line.index(name) + 1}. #{name}."
+    end
+    
+  end
+end
