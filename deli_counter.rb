311fc3c206a4f2a9970dbc51456ac7603528bee0
malorie16 <malorie18@gmail.com>
2018-05-04 20:11:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,12 +11,6 @@ def line(katz_deli_line)
 end
 
 def take_a_number(katz_deli_line, name)
-  if katz_deli_line.length == 0
-    line = "Welcome #{name}. You are currently number"
-       katz_deli_line.each.with_index(1) do |person, i|
-         line << " #{i} in line."
-  
-    return katz_deli_line << name
-  end
-
+    katz_deli_line << name
+    puts "Welcome, #{name}. You are number #{katz_deli_line.length} in line."
 end
